267389e8b8761e17dd5d403f7fefabd625231ed9
Mark Koester <mwkteacher@gmail.com>
2011-10-21 02:49:42 +0800

    removed code from master branch, added readme

---
+++ README.txt
@@ -0,0 +1,5 @@
+Quizlet for Drupal
+==============================================
+Quizlet for Drupal uses the Quizlet API to search and add Quizlet vocabulary and flashcards within your Drupal Site
+
+This is the master branch. Please refer to the 6.x branch. 
--- templates/embedded_cards.tpl.php
+++
@@ -1,4 +0,0 @@
-<div style="background:#fff;padding:3px">
-  <iframe src="<?php print $url; ?>" height="<?php print $height; ?>" width="100%" style="border:0;" scrolling="no"></iframe>
-</div>
-<div class="quizlet_link"><a href="http://quizlet.com/"><b>Powered by Quizlet.com</b></a></div>
