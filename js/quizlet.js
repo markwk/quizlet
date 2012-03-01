abe53e61463a246871adcc6514eba918f5e86e0e
Mark Koester <markwkoester@gmail.com>
2012-03-01 18:24:28 +0800

    Issue: [#1353290]: Port Quizlet to Drupal 7

--- js/quizlet.js
+++ js/quizlet.js
@@ -1,21 +1,23 @@
-$(document).ready(function() {
-  $('#edit-select-card').change(function() {
-    if ($(this).val()) {
-      window.location = $(this).val();
+(function ($) {
+  $(document).ready(function() {
+    $('#edit-select-card').change(function() {
+      if ($(this).val()) {
+        window.location = $(this).val();
+      }
+    });
+    if ($('.quizet_list img').length <= 0) {
+      $('#quizlet_images').hide();  
     }
+    $('#quizlet_images').click(function() {
+      if ($(this).html() == 'Hide Images') {
+        $(this).html('Display Images');  
+        $('.quizet_list img').hide();
+      }
+      else {
+        $(this).html('Hide Images');
+        $('.quizet_list img').show();
+      }
+      return false;
+    });
   });
-  if ($('.quizet_list img').length <= 0) {
-    $('#quizlet_images').hide();  
-  }
-  $('#quizlet_images').click(function() {
-    if ($(this).html() == 'Hide Images') {
-      $(this).html('Display Images');  
-      $('.quizet_list img').hide();
-    }
-    else {
-      $(this).html('Hide Images');
-      $('.quizet_list img').show();
-    }
-    return false;
-  });
-});
+})(jQuery);
